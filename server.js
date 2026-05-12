d541d7f949b9c521e4b1ff6bfbd6145ba74ef480
Shreya <shreyapatilkkw@gmail.com>
2026-05-13 00:15:57 +0530

    Fix PORT fallback

--- server.js
+++ server.js
@@ -67,10 +67,10 @@ app.use('/api/upload', require('./src/routes/upload'));
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('MongoDB Connected');
-    app.listen(process.env.PORT, () => {
-      console.log('Server running on port ' + process.env.PORT);
-    });
-  })
+    const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+});
   .catch((err) => {
     console.log('MongoDB Error:', err.message);
   });
