887f207576a1eeceaaedf217d59429db99ad1a96
Shreya <shreyapatilkkw@gmail.com>
2026-05-13 00:41:13 +0530

    Fix syntax error in server.js

--- server.js
+++ server.js
@@ -68,9 +68,10 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('MongoDB Connected');
     const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log('Server running on port ' + PORT);
-});
+    app.listen(PORT, () => {
+      console.log('Server running on port ' + PORT);
+    });
+  })
   .catch((err) => {
     console.log('MongoDB Error:', err.message);
   });
